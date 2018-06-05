cd2343025e6ea2d3530f366fea79b2a0399eb4f9
OceanicSix <byan0007@student.monash.edu>
2018-06-05 22:40:00 +1000

    rer

---
+++ src/Int_overflow/Int_overflow.java
@@ -0,0 +1,15 @@
+package Int_overflow;
+
+public class Int_overflow {
+    public static void name() {
+
+    }
+
+    public static void main(String[] args) {
+         int value1 = 2147483647;
+        System.out.println("Value 1: " + (value1 + 1));
+
+        int value2 = -2147483648;
+        System.out.println("Value 2: " + (value2 - 1));
+    }
+}
